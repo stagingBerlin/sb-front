8fbf0ee0dc9fcfeb84bd4d88155f11c3df07745e
Habid B Espinosa <hab.bad@hotmail.com>
2021-10-26 15:43:27 +0200

    context, axios react-router package, login signup created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Navigation from './Components/Navigation'
+import { Switch, Route } from 'react-router-dom'
+import Login from './Components/auth/Login'
+import Signup from './Components/auth/Signup'
+
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Switch>
+        <Route path="/login" exact component={Login} />
+        <Route path="/signup" exact component={Signup} />
+      </Switch>
     </div>
   );
 }
