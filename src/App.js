33eed4afccd65dc9e59998f266ce3cb4204134d4
DanMat7 <daniel.matczak@network.rca.ac.uk>
2021-10-28 11:11:12 +0200

    cleaned up old components from App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import Navigation from './Components/Navigation'
-import UserDashBoard from './Components/dashboard/UserDashboard'
-
 function App() {
   return (
     <div className="App">
-      <Navigation/>
-      <UserDashBoard/>
+      App
     </div>
   );
 }
--- src/Components/userDashboard/UserDashboard.js
+++ src/Components/userDashboard/UserDashboard.js
@@ -3,7 +3,7 @@ import React from 'react'
 function UserDashboard() {
     return (
         <div>
-            Userdashboard
+            User dashboard
         </div>
     )
 }
