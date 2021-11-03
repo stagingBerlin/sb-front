177f9e30420d59f8534947182bff4f14d97d9b38
DanMat7 <daniel.matczak@network.rca.ac.uk>
2021-11-03 15:58:37 +0100

    styled in dashboard/ wrote new scss styles

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>
-            Footer
+        <div className='grid-container border-top'>
+            <div className='grid-col-9'><div className='border-left'>New to Berlin?</div></div>
+            <div><div className='border-left'>Want to invest?</div></div>
         </div>
     )
 }
--- src/Components/userDashboard/UserDashboard.js
+++ src/Components/userDashboard/UserDashboard.js
@@ -6,29 +6,40 @@ import DashboardProjectsFeatured from "./DashboardProjectsFeatured";
 import DashboardNetwork from "./DashboardNetwork";
 
 function UserDashboard() {
-
   return (
     <div>
       <div className="grid-container">
         <div className="grid-col-2">
           <img src="" alt="" />
         </div>
-        <div className="grid-col-2">available</div>
-        <button className="button-grid-2fr grid-col-2">Edit Profile</button>
-        <button className="button-grid-2fr grid-col-2">Create Project</button>
-        <div className="grid-col-2">
-          <div>Network</div>
-          <DashboardNetwork />
+        <div className="grid-container-left grid-col-span-2 grid-col-2 border-right ">
+          <div className="grid-col-1 grid-col-span-2">available</div>
+          <button className="button-grid-2fr grid-col-1 border-right-none">
+            Edit Profile
+          </button>
+          <button className="button-grid-2fr grid-col-1 border-right-none">
+            Create Project
+          </button>
+          <div className="grid-col-1 grid-col-span-2">
+            <div className="heading-centered-grid">
+              <div>Network</div>
+            </div>
+            <DashboardNetwork />
+          </div>
+          <div className="grid-col-1 grid-col-span-2">
+            <div className="heading-centered-grid">
+              <div>Current Project</div>
+            </div>
+            <DashboardProjectsCurrent />
+          </div>
         </div>
-        <div className="grid-col-2">
-          <div>Current Project</div>
-          <DashboardProjectsCurrent />
-        </div>
-        <div className="grid-col-4 grid-col-span-5">
+        <div className="grid-col-4 grid-col-span-5 grid-row-1 justify-items-end">
           <DashboardFeed />
         </div>
-        <div className="grid-col-9">
-          <DashboardProjectsFeatured />
+        <div className="grid-container-right grid-col-span-4">
+          <div className="grid-col-span-4">
+            <DashboardProjectsFeatured />
+          </div>
         </div>
       </div>
     </div>
