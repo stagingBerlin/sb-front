aa4c36d0e4c04e3a8a6d4acfe666e068fa1641a8
DanMat7 <daniel.matczak@network.rca.ac.uk>
2021-11-09 13:42:56 +0100

    changed navbar/footer and wrote mixins

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,13 +2,15 @@ import React from "react";
 
 function Footer() {
   return (
-    <div>
-      <div className="grid-container border-top">
-        <div className="grid-col-9">
-          <div className="border-left">New to Berlin?</div>
-        </div>
-        <div>
-          <div className="border-left">Want to invest?</div>
+    <div className="footer">
+      <div className="grid-container ">
+        <div className="grid-footer grid-col-span-10 border-top grid-col-2">
+          <div className="grid-col-8">
+            <div className="border-left ">New to Berlin?</div>
+          </div>
+          <div>
+            <div className="border-left">Want to invest?</div>
+          </div>
         </div>
       </div>
       <div className="deko-bars-container-footer">
