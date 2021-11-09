a339e18eb7e5e691b9a2a1005b1efb22472782f7
Shinhee Chae <shinheechae@gmail.com>
2021-11-09 09:22:26 +0100

    ready to push

---
+++ src/Components/projectsOwn/CreateProjectsOwn.js
@@ -0,0 +1,73 @@
+import React from 'react'
+
+const CreateProjectsOwn = () => {
+    return (
+        <>
+          {/* <form onSubmit={handleSubmit}>
+            <div className="avatar">
+              <label className="avatar__label" htmlFor="avatar">
+                <img
+                  className="avatar__img"
+                  width="100"
+                  height="100"
+                  src={
+                        avatarPreview
+                      ? avatarPreview
+                      : user.avatar
+                  }
+                  alt="avatar"
+                />
+              </label>
+              <input
+                id="avatar"
+                name="avatar"
+                className="avatar__file"
+                type="file"
+                accept="image/*"
+                onChange={(e) => avatarChange(e)}
+              />
+            </div>
+              <label htmlFor="name">Name: </label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                //value={update.name}
+                placeholder={user.name}
+                onChange={handleInput}
+              />
+              <label for="email">Email: </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                value={user.email}
+                disabled
+              />
+              <label htmlFor="username">Username: </label>
+              <input
+                name="username"
+                type="text"
+                id="username"
+                //value={update.username}
+                placeholder={user.username}
+                onChange={handleInput}
+              />
+
+            <MultipleSelect
+              jobName={jobName} 
+              setJobName={setJobName} 
+              jobId={jobId} 
+              setJobId={setJobId}
+              jobs={jobs} 
+              setJobs={setJobs}
+              user={user}
+            />
+            
+            <input type="submit" value="UPDATE" className="button-grid-2fr grid-col-2" />
+          </form> */}
+        </>
+    )
+}
+
+export default CreateProjectsOwn
