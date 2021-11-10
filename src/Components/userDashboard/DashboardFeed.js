62873e4d8948f3b5ab51e63b703c45b0a06299a3
DanMat7 <daniel.matczak@network.rca.ac.uk>
2021-11-10 11:33:08 +0100

    worked on dashboardlayout

--- src/Components/userDashboard/DashboardFeed.js
+++ src/Components/userDashboard/DashboardFeed.js
@@ -3,9 +3,14 @@ import React from "react";
 function DashboardFeed() {
   return (
     <div>
-      <div className="grid-container-feed">
-        <textarea className='post-textarea grid-col-span-5 justify-content-end' rows="4"></textarea>
-        <button className="button-grid-2fr grid-col-4">Post</button>
+      <div className="post-container">
+        <div className="grid-container-feed">
+          <textarea
+            className="post-textarea grid-col-span-5 justify-content-end"
+            rows="4"
+          ></textarea>
+          <button className="button-grid-2fr grid-col-4">Post</button>
+        </div>
       </div>
     </div>
   );
