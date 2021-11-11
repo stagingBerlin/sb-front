b85f193f1c5294125b5ee8b34b23e34512c16c3e
DanMat7 <daniel.matczak@network.rca.ac.uk>
2021-11-11 11:26:32 +0100

    fixed landingpage buttons/ started on carousel

--- src/Components/landingPage/ProjectsCarousel.js
+++ src/Components/landingPage/ProjectsCarousel.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import FeaturedProjects from './FeaturedProjects'
+import { elementAcceptingRef } from "@mui/utils";
+import React, { useEffect, useRef, useState } from "react";
+import FeaturedProjects from "./FeaturedProjects";
 
 function ProjectsCarousel() {
-    return (
-        <div>
-            
-        </div>
-    )
+  // finding width of carousel container to get the value for transition
+  const carouselRef = useRef();
+  const [carouselWidth, setCarouselWidth] = useState();
+  const getCarouselWidth = () => {
+    const newWidth = carouselRef.current.clientWidth;
+    setCarouselWidth(newWidth);
+  };
+  useEffect(() => {
+    getCarouselWidth();
+  }, []);
+  useEffect(() => {
+    window.addEventListener("resize", getCarouselWidth);
+  }, []);
+
+  return (
+    <div>
+      <div>{carouselWidth}</div>
+      <div className="carousel-container" ref={carouselRef}>
+        <FeaturedProjects translate={carouselWidth}/>
+      </div>
+    </div>
+  );
 }
 
-export default ProjectsCarousel
+export default ProjectsCarousel;
