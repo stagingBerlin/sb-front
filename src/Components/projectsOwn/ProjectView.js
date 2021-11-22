0fa97b5435d196816df0982047d22b299e5374c3
Habid B Espinosa <hab.bad@hotmail.com>
2021-11-22 17:00:34 +0100

    project creation styles started

---
+++ src/Components/projectsOwn/ProjectView.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function ProjectView({
+    newProject,
+    setNewProject
+}) {
+    return (
+        <div className="project-view">
+            <div className="card">
+                <div className="card__picture"></div>
+                
+                <h4 className="card__heading">
+                    <span className="card__heading-span">
+                        Your project now add it
+                    </span>
+                </h4>
+                <div className="card__body">
+
+                    
+                    <h6 className="card__concept">
+                        Concept:
+                        <span className="card__concept-span">Habid Badillo</span>
+                    </h6>
+                    
+
+                    <div className="card__description">
+                        <h2 className="card__description--heading">Description:</h2>
+                        <p className="card__description--text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta quibusdam tenetur, non delectus ea sit laborum cupiditate sint ipsa nisi illo, reprehenderit veritatis pariatur suscipit placeat quae voluptatum. Quisquam, consequatur? Architecto deserunt fugiat totam molestiae, fugit, amet dolor nemo ad minima at maiores! Excepturi hic non, dolorem voluptates a, tempore repudiandae beatae voluptatibus id veniam impedit fugit assumenda, voluptas quaerat enim nisi consequatur ullam officia eveniet eligendi quos repellendus quas quam! Rem velit, ea consequatur facilis, necessitatibus laboriosam, totam ipsum deserunt asperiores nulla repellat porro animi odit quisquam ex aut et. Explicabo fugiat minima iusto totam aspernatur repellendus molestias nostrum neque maxime quaerat at impedit, tenetur eius dignissimos id voluptatem! Doloribus deleniti fugiat officia quo modi repellendus quos numquam sunt? Nesciunt nihil aperiam fuga aspernatur. Omnis soluta vel, reprehenderit, dicta magni animi minima expedita sequi illo neque voluptates est nihil ipsa sint nobis aut quia et ex modi eligendi. Quisquam dolorem fugit, accusantium exercitationem perspiciatis error cumque harum maxime vel reprehenderit soluta expedita sunt porro doloribus ad quas, omnis minima adipisci atque. Deserunt, ratione. Sint aperiam, aliquam quibusdam voluptatibus est quam labore voluptas magni veniam aliquid vitae voluptatem vero nobis minus ullam, nulla maxime! Quisquam reiciendis atque blanditiis quidem explicabo.</p>
+                    </div>
+
+                    <div className="card__jobList"></div>
+
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+// Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum amet atque culpa sunt sint recusandae natus reprehenderit incidunt odit et aperiam, nam modi quae vel facere mollitia id non repellat? Aperiam, temporibus perspiciatis cumque sed, veritatis ullam suscipit cum quis laboriosam voluptate deserunt expedita laudantium delectus explicabo quas blanditiis quasi minus eius sint! Dolore assumenda natus vitae voluptas laboriosam hic, eius tempora in molestias alias, vero cum voluptate nesciunt, autem necessitatibus delectus. Distinctio libero, molestias aperiam omnis ad, autem voluptatem, accusantium harum pariatur velit officiis voluptates vitae nulla excepturi consequatur quis possimus. Consectetur, cumque harum. Dignissimos cumque magni qui eaque veritatis ipsam sequi sint minus laborum totam odio in consequatur maxime tenetur nemo, perferendis, saepe voluptatem quasi dolor? Quos eos quam, sint facere ut quibusdam illum minus. Eos, magnam itaque illo quaerat magni accusamus ducimus, consequuntur hic distinctio ipsum voluptates a maxime quis temporibus error totam cupiditate amet deleniti. Facere autem quia accusamus, quibusdam, cupiditate deserunt veniam culpa distinctio nihil temporibus tempore ea. Perspiciatis odio molestiae voluptatibus distinctio excepturi ratione iure reprehenderit cum recusandae quia sint ea fuga itaque pariatur labore asperiores, vero nobis laborum, in harum illum soluta, eligendi blanditiis! Nesciunt, libero aliquam optio repudiandae officiis unde. Aliquam, qui!
+
