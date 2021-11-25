080d885840205e8be344940c9d4e504695d7921d
Shinhee Chae <shinheechae@gmail.com>
2021-11-25 12:45:55 +0100

    string search updated

--- src/Components/projectsOther/ShowProjects.js
+++ src/Components/projectsOther/ShowProjects.js
@@ -2,17 +2,64 @@ import React, { useState, useContext, useEffect } from "react";
 import { UserContext } from "../../context/UserContext";
 import { getProjects } from "../../helpers/apiCalls";
 import { Link } from "react-router-dom";
+import { styled, alpha } from "@mui/material/styles";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
+
+//* ////////////////// MUI ///////////////////////////
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.black, 0.08),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(0),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+//* ///////////////////////////////////////////////
 
 function ShowProjects() {
   const { user, projects, setProjects, viewProject, setViewProject } =
     useContext(UserContext);
   const [isMyRole, setIsMyRole] = useState(false);
+  const [keyword, setKeyword] = useState("");
   const [showWhat, setShowWhat] = useState("none");
-  
+  const [errorMsg, setErrorMsg] = useState("");
+
   useEffect(() => {
     const myRole = user.profession.map((role) => role.title);
 
@@ -55,13 +102,11 @@ function ShowProjects() {
       setViewProject(newest);
     } else if (!isMyRole && showWhat === "urgent") {
       const urgent = [...projects].sort((x, y) => {
-       
         return new Date(x.deadline) - new Date(y.deadline);
       });
       setViewProject([...urgent]);
     } else {
       setViewProject(projects);
-    
     }
   }, [isMyRole, showWhat]);
 
@@ -73,6 +118,29 @@ function ShowProjects() {
     setIsMyRole(!isMyRole);
   }
 
+  function textSearch(e) {
+    e.preventDefault();
+    const text = keyword.toLowerCase();
+    setErrorMsg("");
+    const filtered = viewProject.filter((item) => {
+      return (
+        item.authorship.toLowerCase().includes(text) ||
+        item.title.toLowerCase().includes(text) ||
+        item.description.toLowerCase().includes(text)
+      );
+    });
+
+    if (keyword && filtered.length === 0) {
+      setErrorMsg(`No projects found under "${keyword}".`);
+    } else if (keyword && filtered) {
+      setErrorMsg("");
+      setViewProject([...filtered]);
+    } else {
+      setErrorMsg("");
+      setViewProject(projects);
+    }
+  }
+
   return (
     <>
       <div className="grid-container">
@@ -81,18 +149,19 @@ function ShowProjects() {
           style={{ marginTop: "3rem" }}
         >
           <h1>All Projects</h1>
-          <div>
-            <label htmlFor="isMyRole" style={{ marginRight: ".5%" }}>
-              Show projects containing my role(s) only
-            </label>
-            <input
-              type="checkbox"
-              name="isMyRole"
-              checked={isMyRole}
-              onChange={filterByRole}
-              style={{ marginRight: "2%" }}
-            />
-
+          <div style={{ display: "flex" }}>
+            <div style={{ width: "100%" }}>
+              <label htmlFor="isMyRole" style={{ marginRight: ".5%" }}>
+                Show projects containing my role(s) only
+              </label>
+              <input
+                type="checkbox"
+                name="isMyRole"
+                checked={isMyRole}
+                onChange={filterByRole}
+                style={{ marginRight: "2%" }}
+              />
+            </div>
             <FormControl sx={{ m: -1, minWidth: 80, height: 20 }} size="small">
               <InputLabel id="demo-simple-select-helper-label">
                 Sort by
@@ -109,19 +178,32 @@ function ShowProjects() {
                 <MenuItem value="urgent">Closest Deadlines</MenuItem>
               </Select>
             </FormControl>
+
+            <form onSubmit={textSearch}>
+              <Search sx={{ mt: -1, mx: "auto", width: 4, height: 30 }}>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search…"
+                  inputProps={{ "aria-label": "search" }}
+                  onChange={(e) => setKeyword(e.target.value)}
+                />
+              </Search>
+            </form>
           </div>
         </div>
-
+        {errorMsg ? (
+          <h2 className="grid-col-2 grid-col-span-10">{errorMsg}</h2>
+        ) : null}
         <div className="grid-col-2 grid-col-span-10" id="project-grid">
           {viewProject.map((project, i) => (
             <div style={{ marginTop: "3rem" }} key={i}>
-              {/* <EinzelProjectOther /> */}
               <Link
                 style={{ textDecoration: "none" }}
                 to={`/account/allprojects/${project._id}`}
                 state={{ viewProject: [] }}
                 className="button-link"
-              
               >
                 <img
                   src={project.images[0]}
@@ -141,4 +223,3 @@ function ShowProjects() {
 }
 
 export default ShowProjects;
-
