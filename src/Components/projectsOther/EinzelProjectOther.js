71acbfd1f83c68bc943f0786faff9188a6f65cc6
Shinhee Chae <shinheechae@gmail.com>
2021-11-29 17:27:22 +0100

    project other single view updated

--- src/Components/projectsOther/EinzelProjectOther.js
+++ src/Components/projectsOther/EinzelProjectOther.js
@@ -4,6 +4,7 @@ import { Link, useLocation } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
+import { Hidden } from "@mui/material";
 
 //******** MUI ********//
 const style = {
@@ -25,6 +26,9 @@ const EinzelProjectOther = (id) => {
   const { user, projects, setProjects, viewProject, setViewProject } =
     useContext(UserContext);
 
+  const handleApply = () => {};
+  const handleBookmark = () => {};
+
   //******** MUI ********//
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => {
@@ -36,52 +40,69 @@ const EinzelProjectOther = (id) => {
   //*********************/
 
   return (
-    <div className="grid-container">
-      <div className="grid-col-2 grid-col-span-4">
-        {viewProject &&
-          viewProject
-            .filter((p) => p._id === id.id)
-            .map((item) => (
-              <div key={item._id}>
-                <h2>{item.title}</h2>
-                <p>Concept by {item.authorship}</p>
-                <p>Description: {item.description}</p>
-                <p>
-                  Roles:
-                  {item.jobList
-                    .map((role) => {
-                      return role.job.title
-                    })
-                    .join(" ∙ ")}
-                    <Button onClick={handleOpen}>Show Description & Apply</Button>
-                  <Modal
-                    hideBackdrop
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="child-modal-title"
-                    aria-describedby="child-modal-description"
+    <>
+      <div className="grid-container" style={{ overflow: "hidden" }} height="auto">
+        <div className="grid-col-6 grid-col-span-8">
+          {viewProject &&
+            viewProject
+              .filter((p) => p._id === id.id)
+              .map((item) => (
+                <div key={item._id}>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      margin: "2rem 0 1rem",
+                    }}
                   >
-                    <Box sx={{ ...style, width: 200 }}>
-                      <h2 id="child-modal-title">Text in a child modal</h2>
-                      <p id="child-modal-description">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit.
-                      </p>
-                      <Button onClick={handleClose}>Close</Button>
-                    </Box>
-                  </Modal>
-                </p>
-                <p>Participants: {item.jobList.participant}</p>
-                <p>Deadline: {item.deadline?.substr(0, 10)} </p>
-                <p>Starting on: {item.starting?.substr(0, 10)} </p>
-                <p>Contact: {user.username}</p>
-                <p>
-                  Project status: {item.isHiring ? "Hiring now" : "Not hiring"}
-                </p>
-              </div>
-            ))}
+                    <h2>{item.title}</h2>
+                    <h3>Concept by {item.authorship}</h3>
+                  </div>
+                  <div >
+                    <img src={item.images} width="auto"></img>
+                    <p style={{ padding: "1rem 0" }}>Description: {item.description.substr(1, 400)}...<Button onClick={handleBookmark}> Bookmark </Button><Button onClick={handleBookmark}> Share </Button></p>
+                    <p>
+                      Roles:{" "}
+                      {item.jobList.map((role) => (
+                        <Button onClick={handleOpen}> {role.job.title} </Button>
+                      ))}{" "}
+                      (click each role to read descriptions & apply)
+                      <Modal
+                        hideBackdrop
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="child-modal-title"
+                        aria-describedby="child-modal-description"
+                      >
+                        <Box sx={{ ...style, width: 400 }}>
+                          {item.jobList.map((role) => (
+                            <>
+                              <h3 id="child-modal-title">
+                                {" "}
+                                {role.jobDescription}
+                              </h3>
+                            </>
+                          ))}
+
+                          <Button onClick={handleApply}>Apply</Button>
+                          <Button onClick={handleClose}>Close</Button>
+                        </Box>
+                      </Modal>
+                    </p>
+                    <p>Participants: {item.jobList.participant}</p>
+                    <p>Deadline: {item.deadline?.substr(0, 10)} </p>
+                    <p>Starting on: {item.starting?.substr(0, 10)} </p>
+                    <p>Contact: {user.username}</p>
+                    <p>
+                      Project status:{" "}
+                      {item.isHiring ? "Hiring now" : "Not hiring"}
+                    </p>
+                  </div>
+                </div>
+              ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
